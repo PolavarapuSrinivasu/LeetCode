b92e8a677700e9b87f988e68c96fa549b568975e
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2026-02-07 20:36:20 +0530

    Time: 83 ms (5.33%) | Memory: 13.9 MB (65.43%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]!=0) continue;
+            for(int j=arr.size()-1;j>i;j--) {
+                swap(arr[j],arr[j-1]);
+            }
+            arr[i] = 0;
+            i+=1;
+        }
+    } 
+};
