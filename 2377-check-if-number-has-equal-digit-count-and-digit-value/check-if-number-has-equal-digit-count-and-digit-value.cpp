503998d1fcf6f4dba1921b5808e8f9947195da10
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2026-03-02 09:42:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (25.95%) - LeetSync

---
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool digitCount(string num) {
+        unordered_map<int,int>freq;
+        for(auto digit : num) freq[digit-'0']++;
+
+        for(int i=0;i<num.size();i++){
+            if((num[i]-'0')!=freq[i]) return false;
+        }
+
+        return true;
+    }
+};
