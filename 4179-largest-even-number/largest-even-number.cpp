f8f3e68cd3c9dae718b71030ad9e584d2c830d62
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2026-03-19 18:31:49 +0530

    Time: 1 ms (15.67%) | Memory: 8.6 MB (69.82%) - LeetSync

---
+++ 4179-largest-even-number/largest-even-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string largestEven(string s) {
+        while(!s.empty()) {
+            if((s.back()-'0')%2==0) return s;
+            s.pop_back();
+        }
+
+        return "";
+    }
+};
