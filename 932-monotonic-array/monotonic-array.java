c69e0f58d474da5af252f88eefa8ab7378e792ba
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2026-08-05 19:06:36 +0530

    Time: 3 ms (19.30%) | Memory: 85.1 MB (81.06%) - LeetSync

--- 932-monotonic-array/monotonic-array.java
+++ 932-monotonic-array/monotonic-array.java
@@ -10,18 +10,16 @@ class Solution {
 
         if(s == nums.length - 1) return true;
 
-        if(nums[s] <= nums[s + 1]) {
-            for(int i = s; i < nums.length - 1; i++) {
-                if(nums[i] > nums[i+1]) return false;
-            }
+        int i = 0;
+        int d = 0;
+
+        for(int j = s; j < nums.length - 1; j++) {
+            if(nums[j] < nums[j+1]) i++;
+            else if(nums[j] > nums[j+1]) d++;
+            else continue;
         }
 
-        if(nums[s] >= nums[s + 1]) {
-            for(int i = s; i < nums.length - 1; i++) {
-                if(nums[i] < nums[i+1]) return false;
-            }
-        }
-
-        return true;
+        if(i == 0 || d == 0) return true;
+        else return false;
     }
 }
